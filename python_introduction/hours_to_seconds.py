5932c8ce2c2631ca64138837978b8d81f2f6ceae
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 21:48:44 -0600

    new commits from wk3 project_3

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,14 @@
+#definition of variables
+
+hours = 2       #this defines the number of hours yu intend to convert to seconds
+
+#Doing the actual conversion to seconds
+
+seconds = hours*60*60
+
+#lets print out the result
+
+print(f"{hours} hour(s) is {seconds} seconds")
+
+
+
