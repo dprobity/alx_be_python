0f69dbfcc81acbb93dfe9a20ff3a060e818c4368
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 21:37:17 -0600

    new commits from wk3 project_1

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+#declaration of variables
+
+principal = 1000   #representing $1000
+rate = 0.05     #representing 5% annual interest
+time = 3      #representing 3 years
+
+#calculating the simple interest
+
+interest = principal * rate * time
+
+#print the result
+
+print(f"The simple interest is: {interest}")
+
