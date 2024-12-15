2b0aee10279f2ec545b33ffd355791c002394185
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 22:18:34 -0600

    new commits from wk3 project_4

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,19 @@
+# importing modules
+
+import sys
+
+#request for user age as commandline input
+
+age = int(input("How old are you?"))
+#age = int(sys.argv[1])
+
+
+# Calculating how old the person will be by 2050
+
+age_in_2050 = age + 27
+
+#lets print out the users age in 2050
+
+print(f"In 2050, you will  be {age_in_2050} years old")
+
+
