6b81b27ec3cfcbfc37d273a2458e7961b38ee37a
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 21:17:43 -0600

    new commits from wk3 project_0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1, number2 = 10, 5
+sum_= number1 + number2
+difference = number1 - number2
+product = number1*number2
+
+print("Addition of", number1, "and", number2, "is", sum_)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
