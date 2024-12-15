44b870d27a85bacd4328bd950d18ca701207a555
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 21:27:35 -0600

    new commits from wk3 project_0

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,22 @@
+#assignment of variables
+
 number1 = 10
 number2 = 5
+
+#perform calculations 
+
 sum_= number1 + number2
 difference = number1 - number2
 product = number1*number2
 
-print("Addition of", number1, "and", number2, "is", sum_)
-print("Subtraction of", number1, "and", number2, "is", difference)
-print("Multiplication of", number1, "and", number2, "is", product)
+#print results using f-string
+
+print(f"Addition of {number1} and {number2} is {sum_} ")
+print(f"Subtraction of {number1} and {number2} is {difference} ")
+print(f"Multiplication of {number1} and {number2} is {product}")
+
+
+
+#print("Addition of", number1, "and", number2, "is", sum_)
+#print("Subtraction of", number1, "and", number2, "is", difference)
+#print("Multiplication of", number1, "and", number2, "is", product)
