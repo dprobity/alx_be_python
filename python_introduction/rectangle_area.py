22676dc3f464f62f7c72f183aada68e81928b8a0
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-14 21:42:09 -0600

    new commits from wk3 project_2

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,13 @@
+#definition of variables
+
+length = 10
+width = 5
+
+#calculating the area of the rectangle
+
+area = length * width
+
+#printing the calculated area
+
+print(f"The area of the rectangle is: {area}")
+
