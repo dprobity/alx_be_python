0dbf763c4a5dd74165efdf3429003a9baa8337e6
Emehelu Amobi David <dprobityconcept@gmail.com>
2024-12-15 23:47:48 -0600

    new commits from wk3 project_5

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,23 @@
+# This script will calculate the users monthly savings based on based on inputted monthly income and exepenses, it will the project this savings over a year, assumming a fixed interest rate, to demonstrate compound intereste's effect on savings
+
+#prompt the user for input
+
+income = int(input("Enter your monthly income:"))
+expenses = int(input("Enter your total monthly expenses:"))
+
+
+# Calculating the monthly savings
+
+savings = income - expenses
+
+# Calculating the projected annual savings 
+
+projected_annual_savings = savings * 12 + (savings * 12  * 0.05)
+
+
+# Printing the results
+
+print(f"Your monthly savings are ${savings}. ")
+print(f"Projected savings afetr one year, with interest, is: ${projected_annual_savings}.")
+
+
