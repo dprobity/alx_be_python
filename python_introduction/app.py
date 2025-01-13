693ee08b9e424d822b27bab4182c7f22b128cf6c
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-01-13 03:19:49 -0600

    clean slate

--- python_introduction/app.py
+++
@@ -1,5 +0,0 @@
-age = 22
-
-message = "Eligible" if age >= 18 else "Ineligible"
-
-print(message)
--- python_introduction/hekko.py
+++
@@ -1,9 +0,0 @@
-score = int(input("Enter your score: "))
-
-if score >= 90:
-    print("Grade A")
-elif score >= 75:
-    print("Grade B")
-else:
-    print("Grade C")
-
